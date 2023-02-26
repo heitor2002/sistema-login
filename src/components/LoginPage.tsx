ad33e5e5e6ee0d94bc68ddfdf104f188e3aa73ad
heitor2002 <montagnini.heitor@gmail.com>
2023-02-26 20:38:12 -0300

    feat: função para obter dados do form.

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 const LoginPage = () => {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
+    const handleSubmit = (e:FormEvent) => {
+        e.preventDefault()
+        console.log(email, password)
+    }
     return (
         <>
         <div className="container">
             <div className="box-login">
                 <h2>Página de login</h2>
-                <form id="form">
+                <form id="form" onSubmit={handleSubmit}>
                     <div className="field">
                         <label>E-mail:</label>
                         <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
