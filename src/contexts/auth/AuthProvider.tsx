09cea0f3919f479d248cf9a0c02b46093dee3917
heitor2002 <montagnini.heitor@gmail.com>
2023-02-27 10:55:45 -0300

    feat: criação de provider.

--- src/contexts/auth/AuthProvider.tsx
+++ src/contexts/auth/AuthProvider.tsx
@@ -1,5 +1,15 @@
-const AuthProvider = () => {
-    return
-}
+import { useState } from "react";
+import { User } from "../../types/User";
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }: { children: JSX.Element }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ user, signin, signout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthProvider;
