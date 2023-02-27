981002e75580564cfd8ed6cb3f3163c95e9f94eb
heitor2002 <montagnini.heitor@gmail.com>
2023-02-27 11:29:10 -0300

    feat: criação de página privada.

---
+++ src/components/PrivatePage.tsx
@@ -0,0 +1,11 @@
+const PrivatePage = () => {
+    return (
+        <>
+        <div className="container">
+            <h2>Parabéns usuário, logado com sucesso!</h2>
+        </div>
+        </>
+    )
+}
+
+export default PrivatePage;
