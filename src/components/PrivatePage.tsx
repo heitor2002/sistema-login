d88fb78b042acb19eae43cd50735fc352f88f01b
heitor2002 <montagnini.heitor@gmail.com>
2023-02-27 15:23:36 -0300

    style: estilização de página privada.

--- src/components/PrivatePage.tsx
+++ src/components/PrivatePage.tsx
@@ -1,20 +1,25 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/auth/AuthContext";
+import { Link } from "react-router-dom";
 
 const PrivatePage = () => {
-    const auth = useContext(AuthContext)
-    return (
-        <>
-        <div className="container">
-            <div style={{color: "white"}}>
-            <h2>Parabéns {auth.user?.username}, logado com sucesso!</h2>
-            <h2>Username: {auth.user?.username}</h2>
-            <h2>E-mail: {auth.user?.email}</h2>
-            <h2>Token: {auth.user?.token}</h2>
-            </div>
+  const auth = useContext(AuthContext);
+  return (
+    <>
+      <div className="container">
+        <div className="private-page">
+          <center>
+            <h2>Parabéns {auth.user?.username}, login realizado com sucesso!</h2>
+          </center>
+          <h3>Username: {auth.user?.username}</h3>
+          <h3>E-mail: {auth.user?.email}</h3>
+          <h3>Token: {auth.user?.token}</h3>
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/logout"><button>Sair</button></Link>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default PrivatePage;
