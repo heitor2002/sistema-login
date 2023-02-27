99c884d47815c549a18b00eb8d6eba6eb152c3b9
heitor2002 <montagnini.heitor@gmail.com>
2023-02-27 11:19:28 -0300

    feat: validação da função signin.

--- src/contexts/auth/AuthProvider.tsx
+++ src/contexts/auth/AuthProvider.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import useApi from "../../hooks/useApi";
 import { User } from "../../types/User";
 import { AuthContext } from "./AuthContext";
 
 const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const [user, setUser] = useState<User | null>(null);
-  const signin = (email:string, password:string) => {
+  const api = useApi();
+  const signin = async (email: string, password: string) => {
+    const data = await api.signin(email, password);
+    if (data.user && data.token) {
+      setUser(data.user);
+      return true;
+    }
+    return false;
+  };
 
-  }
-
-  const signout = () => {}
+  const signout = () => {};
 
   return (
     <AuthContext.Provider value={{ user, signin, signout }}>
--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,18 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API
-})
+  baseURL: "http://localhost:3000",
+});
 
 const useApi = () => ({
-    validateToken: async (token: string) => {
+  validateToken: async (token: string) => {
+    const response = await api.post("/users", { token });
+    return response.data;
+  },
+  signin: async (email: string, password: string) => {
+    const response = await api.post("/users", { email, password });
+    return response.data;
+  },
 
-    },
-    signin: async(email:string, password:string) => {
+  logout: async () => {
+    const response = await api.post("/logout");
+    return response.data;
+  },
+});
 
-    }
-
-    logout: async () =>{
-        
-    }
-})
+export default useApi;
